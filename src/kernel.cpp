fef477266ce1a12e358a9dc2f8f5cb0fde59cfca
freakcoderz <42624259+freakcoderz@users.noreply.github.com>
2020-04-10 22:24:17 -0400

    Update stakemodifier checkpoints

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -381,7 +381,7 @@ bool CheckStakeModifierCheckpoints(int nHeight, unsigned int nStakeModifierCheck
 {
     MapModifierCheckpoints& checkpoints = (fTestNet ? mapStakeModifierCheckpointsTestNet : mapStakeModifierCheckpoints);
 
-    //if (checkpoints.count(nHeight))
-       // return nStakeModifierChecksum == checkpoints[nHeight];
+    if (checkpoints.count(nHeight))
+        return nStakeModifierChecksum == checkpoints[nHeight];
     return true;
 }
